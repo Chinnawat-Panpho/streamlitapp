34036cef65700d6ef97d5e080f69b9846e10ac34
Ren-BBS-Thailand <ren@thai.bbs.co.jp>
2026-08-26 13:41:49 +0700

    Added multiple lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-st.write("Hello, World")
+st.write("""
+# Welcome to my first streamlit app!
+
+This is a simple app that demonstrates how to use Streamlit to create interactive web applications with Python. You can use this app to display data, create visualizations, and interact with your data in real-time.
+
+""")
